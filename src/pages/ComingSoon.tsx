b46fb3e30e4815e1686adabd96628a578e6621cb
JackyShen <591481757@qq.com>
2021-09-27 15:54:14 +0800

    feat: languages

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -3,10 +3,11 @@ import Col from '../components/Column'
 import styled, { useTheme } from 'styled-components'
 import { Text } from 'style'
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
-
+import { useTranslation } from 'react-i18next';
 
 const ComingSoon: React.FunctionComponent = (props) => {
   const theme = useTheme();
+  const { t } = useTranslation();
 
   return (
     <Col style={{alignItems: 'center', height: '100vh', justifyContent: 'center', background: theme.colors.background}}>
@@ -14,6 +15,7 @@ const ComingSoon: React.FunctionComponent = (props) => {
         <ClimbingBoxLoader size="5vw" color={theme.colors.primary} loading={true}/>
       </div>
       <Text color={theme.colors.primary} bold fontSize="5vw" mt="15vw">Coming Soon</Text>
+      <Text color={theme.colors.primary} bold fontSize="5vw" mt="3vw">{t('Visit Web')}</Text>
     </Col>
   )
 }
